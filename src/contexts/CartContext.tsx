da1c89f913755a6a6fd2e72dac7b1e35c14e2599
null <null>
2024-09-19 09:07:34 +0530

    Commit

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,24 +1,14 @@
-/*
-  1. Let's create a context using createContext()
-  2. the context can be used to make a provider component 
-  3. create a provider component 
-  4. supply the context data to the provider component -- in App.tsx
-  5. access the context data in any component using useContext -- Header
-*/
 
 import { createContext, useState } from "react";
 import { IProduct } from "../models/IProduct";
 
-// what data will we supply thru the context?
 interface ICartContext {
   cartItems: IProduct[];
   addToCart: (product: IProduct) => void;
 }
 
-// Step 1: create a context
 export const CartContext = createContext<ICartContext | undefined>(undefined);
 
-// Step 2: create a new component -- CartProvider -- use it in App.tsx
 export const CartContextProvider: React.FC<{children: React.ReactNode}> = ({children}) => {
   const [myCartItems, setMyCartItems] =  useState<IProduct[]>([]);
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,21 @@
 // node_modules import
 import React from 'react'; // official react core library 
 import ReactDOM from 'react-dom/client'; // official react dom library - for working with the DOM.
-
+import 'bootstrap/dist/css/bootstrap.css';
 // custom imports
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <App />
-  // </React.StrictMode>
+  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
